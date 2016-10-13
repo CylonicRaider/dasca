45b4d7c39fa075019b061a8d56656cd44b0c97a8
CylonicRaider <CylonicRaider@users.noreply.github.com>
2016-10-13 13:08:14 +0200

    Add DascaAction

--- main.js
+++ main.js
@@ -3,3 +3,39 @@
  * Game implementation */
 
 'use strict';
+
+/* A serializable (and reifiable) function wrapper, usable as a Scheduler
+ * task */
+function DascaAction(name, cb) {
+  if (cb == null && DascaAction.cache[name])
+    cb = DascaAction.cache[name].cb;
+  this.name = name;
+  this.cb = cb;
+}
+
+DascaAction.prototype = {
+  /* OOP helper */
+  constructor: DascaAction
+};
+
+/* Cache of actions
+ * Although the term is not exactly correct, I couldn't think of a better
+ * one. */
+DascaAction.cache = {};
+
+/* Add an action to the cache */
+DascaAction.addAction = function(name, cb) {
+  DascaAction.cache[name] = new DascaAction(name, cb);
+};
+
+/* Serialization hook */
+DascaAction.__save__ = function(action) {
+  return {name: action.name};
+}
+
+/* Deserialization hook */
+DascaAction.__restore__ = function(data) {
+  var ret = new DascaAction(data.name);
+  if (ret.cb == null) throw new Error("Unrecognized action!");
+  return ret;
+}
