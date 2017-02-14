63288b9473cffd95c7139d689235ea9e4c9f8e82
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-02-14 16:26:51 +0100

    Add utilities

--- main.js
+++ main.js
@@ -3,3 +3,71 @@
  * Game implementation */
 
 'use strict';
+
+/* Utilities */
+function $id(id, elem) {
+  return (elem || document).getElementById(id);
+}
+function $sel(sel, elem) {
+  return (elem || document).querySelector(sel);
+}
+function $selAll(sel, elem) {
+  return (elem || document).querySelectorAll(sel);
+}
+
+/* Create a DOM element */
+function $makeNode(tag, className, attrs, children) {
+  /* Allow omitting parameters */
+  if (Array.isArray(className)) {
+    if (! attrs && ! children) {
+      children = className;
+      attrs = null;
+      className = null;
+    }
+  } else if (typeof className == "object" && className != null) {
+    if (! children) {
+      children = attrs;
+      attrs = className;
+      className = null;
+    }
+  } else if (Array.isArray(attrs) || typeof attrs == "string") {
+    if (! children) {
+      children = attrs;
+      attrs = null;
+    }
+  }
+  /* Create node */
+  var ret = document.createElement(tag);
+  /* Set classes */
+  if (className) ret.className = className;
+  /* Set additional attributes */
+  if (attrs) {
+    for (var name in attrs) {
+      if (! attrs.hasOwnProperty(name)) continue;
+      ret.setAttribute(name, attrs[name]);
+    }
+  }
+  /* Add children */
+  if (children) {
+    if (typeof children == "string") children = [children];
+    for (var i = 0; i < children.length; i++) {
+      var e = children[i];
+      if (! e) {
+        /* Allow conditional node omission */
+      } else if (typeof e == "string") {
+        /* Strings become text nodes */
+        ret.appendChild(document.createTextNode(e));
+      } else if (typeof e != "object") {
+        /* Other primitive types are not allowed */
+        throw new Error("Bad child encountered during DOM node creation");
+      } else if (Array.isArray(e)) {
+        /* Arrays are handled recursively */
+        ret.appendChild($makeNode.apply(null, e));
+      } else {
+        /* Everything else is assumed to be a DOM node */
+        ret.appendChild(e);
+      }
+    }
+  }
+  return ret;
+}
