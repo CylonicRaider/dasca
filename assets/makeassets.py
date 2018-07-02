4a2481514c8d70614af4f02957b460e72def3bab
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-07-02 18:54:00 +0200

    Implement basic asset processing

--- assets/makeassets.py
+++ assets/makeassets.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: ascii -*-
 
-import re
+import sys, re
 
-SPECIAL_RE = re.compile(r'/\*!(.*?)\*/')
+SPECIAL_RE = re.compile(r'/\*!(.*+)\*/')
+
+LINE_LENGTH = 78
 
 def parse_input(text):
     index = 0, 0
@@ -17,3 +19,34 @@ def parse_input(text):
         index = m.end()
     if index != len(text):
         yield (None, text[index:])
+
+def import_asset(filename):
+    with open(filename) as f:
+        data = f.read()
+    encdata = base64.b64encode(data.encode('utf-8')).decode('ascii')
+    index = 0
+    while index < len(encdata):
+        endindex = index + LINE_LENGTH - 2
+        yield '\\\n  '
+        yield encdata[index:endindex]
+        index = endindex
+
+def process_input(stream):
+    for item in stream:
+        if item[0] is None:
+            yield item[1]
+        elif item[0] == 'include':
+            if len(item) != 2:
+                raise SystemExit('Invalid include directive: %r' % (item,))
+            for piece in import_asset(item[1]):
+                yield piece
+        else:
+            raise SystemExit('Invalid directive: %r' % (item,))
+
+def main():
+    text = sys.stdin.read()
+    for piece in process_input(parse_input(text)):
+        sys.stdout.write(piece)
+    sys.stdout.flush()
+
+if __name__ == '__main__': main()
